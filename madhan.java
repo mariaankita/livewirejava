a618069c3dd3c8cf0861dfc2e940da03d18a6668
mariaankita <mariaankita08@gmail.com>
2026-06-20 17:00:25 +0530

    Create madhan.java

---
+++ madhan.java
@@ -0,0 +1,273 @@
+package madhan;
+import java.util.Scanner;
+public class madhan {
+
+	public static void main(String[] args)
+	{
+//if else programs
+
+		
+//		pgrm 1
+		
+//		Scanner sc=new Scanner(System.in);
+//      System.out.println("Enter the breath and length:");
+//		int a=sc.nextInt();
+//		int b=sc.nextInt();
+//		if(a==b)
+//		{
+//		  System.out.println("it is square");
+//		}
+//		else
+//		{
+//			System.out.println("it is rectangle");
+//		}
+		
+		
+//		pgrm2
+		
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("Enter a number:");
+//		int a=sc.nextInt();
+//		int b=a*a;
+//		if(b>50)
+//		{
+//			System.out.println("it's square is above 50");
+//		}
+//		else
+//		{
+//			System.out.println("it's square is below 50");
+//		}
+		
+		
+//		pgrm3
+//		
+//		Scanner sc=new Scanner(System.in);
+//      System.out.println("Enter 2 numbers:");
+//		int a=sc.nextInt();
+//		int b=sc.nextInt();
+//		int c=a-b;
+//		if(c==0)
+//		{
+//		  System.out.println("the difference is 0");
+//		}
+//		else
+//		{
+//			System.out.println("the difference is not a zero");
+//		}
+		
+		
+//		prgrm4
+		
+//		Scanner sc=new Scanner(System.in);
+//      System.out.println("Enter a numbers:");
+//		int a=sc.nextInt();
+//		int b=a%10;
+//		int c=a%5;
+//		if(b==0)
+//		{
+//			System.out.println("it is divisible by 10");
+//		}
+//		else if(c==0)
+//		{
+//			System.out.println("it is divisible by 5");
+//		}
+//		else
+//		{
+//			System.out.println("it is not divisible by both");
+//		}
+		
+		
+//		prgrm5
+	
+//		Scanner sc=new Scanner(System.in);
+//      System.out.println("Enter 2 numbers:");
+//		int a=sc.nextInt();
+//		int b=sc.nextInt();
+//		if(a>b)
+//		{
+//			System.out.println(a+" is the biggest digit");
+//		}
+//		else
+//		{
+//			System.out.println(b+" is the biggest digit");
+//		}
+		
+		
+//		prgm6
+		
+//		Scanner sc=new Scanner(System.in);
+//      System.out.println("Enter 2 numbers:");
+//	    int a=sc.nextInt();
+//     	int b=sc.nextInt();
+//     	System.out.println("enter the number of the mathematical operation:");
+//     	System.out.println("1- add , 2- sub , 3- multiply , 4- divide");
+//     	int op=sc.nextInt();
+//     	if(op==1)
+//     	{
+//     		System.out.println(a+b);
+//     	}
+//     	else if(op==2)
+//     	{
+//     		System.out.println(a-b);
+//     	}
+//     	else if(op==3)
+//     	{
+//     		System.out.println(a*b);
+//     	}
+//     	else if(op==4)
+//     	{
+//     		System.out.println(a/b);
+//     	}
+//     	else
+//     	{
+//     		System.out.println("operation not avaliable");
+//     	}
+		
+		
+//		pgrm7
+		
+//		Scanner sc=new Scanner(System.in);
+//        System.out.println("Enter the weight of parcel in gm:");
+//        int a=sc.nextInt();
+//        System.out.println("type of booking:");
+//        System.out.println("o for ordinary & e for express");
+//     	char b=sc.next().charAt(0);
+//     	if(a<=100 & b=='o')
+//     	{
+//     		System.out.println("the price is 80/-");
+//     	}
+//     	else if(a<=100 & b=='e')
+//     	{
+//     		System.out.println("the price is 100/-");
+//     	}
+//     	else if(a<=500 & a>=101 & b=='o')
+//     	{
+//     		System.out.println("the price is 150/-");
+//     	}
+//     	else if(a<=500 & a>=101 & b=='e')
+//     	{
+//     		System.out.println("the price is 200/-");
+//     	}
+//     	else if(a<=1000 & a>=501 & b=='e')
+//     	{
+//     		System.out.println("the price is 250/-");
+//     	}
+//     	else if(a<=1000 & a>=501 & b=='o')
+//     	{
+//     		System.out.println("the price is 210/-");
+//     	} 
+//     	else if(a>1000 & b=='o')
+//     	{
+//     		System.out.println("the price is 250/-");
+//     	}
+//     	else if(a>1000 & b=='e')
+//     	{
+//     		System.out.println("the price is 300/-");
+//     	}
+		
+//		pgrm8
+		
+//		Scanner sc=new Scanner(System.in);
+//        System.out.println("Enter a numbers:");
+//		int a=sc.nextInt();
+//		int b=a%10;
+//		if(b==0)
+//		{
+//			System.out.println("it is divisible by 10");
+//		}
+//		else
+//		{
+//			System.out.println("it is not divisible by 10");
+//		}
+		
+//		 pgrm9
+		
+//		Scanner sc=new Scanner(System.in);
+//        System.out.println("Enter your computer science mark:");
+//		int a=sc.nextInt();
+//		if(a>=50)
+//		{
+//			System.out.println("pass");
+//		}
+//		else
+//		{
+//			System.out.println("failed");
+//		}
+		
+//		pgrm10
+		
+//		Scanner sc=new Scanner(System.in);
+//      System.out.println("Enter a value:");
+//		int a=sc.nextInt();
+//		if(a==1)
+//		{
+//			System.out.println("You can go out and play");
+//		}
+//		else
+//		{
+//			System.out.println("You cannot go out and play");
+//		}
+		
+//		pgrm11
+		
+//		Scanner sc=new Scanner(System.in);
+//	      System.out.println("Enter a value:");
+//			int a=sc.nextInt();
+//			if(a==1)
+//			{
+//				System.out.println("The exam will be easy");
+//			}
+//			else
+//			{
+//				System.out.println("The exam will be difficult");
+//			}
+		
+//		pgrm12
+		
+		
+//			Scanner sc=new Scanner(System.in);
+//		    System.out.println("Enter a value:");
+//		    int a=sc.nextInt();
+//		    int b=a*a;
+//		    int c=b%10;
+//		    if(c==a)
+//		    {
+//		    	System.out.println("Automorphic number");
+//		    }
+//		    else
+//		    {
+//		    	System.out.println("not an Automorphic number");
+//		    }
+		
+//		pgrm13
+		
+//		Scanner sc=new Scanner(System.in);
+//	    System.out.println("Enter a value:");
+//	    int a=sc.nextInt();
+//	    if(a%7==0 || a%10==7)
+//	    {
+//	    	System.out.println("buzz number");
+//	    }
+//	    else
+//	    {
+//	    	System.out.println("not an buzz number");
+//	    }
+	    
+//	    pgrm14
+	    
+//	    Scanner sc=new Scanner(System.in);
+//	    System.out.println("Enter a character:");
+//	    char a=sc.next().charAt(0);
+//	    if(a>=65 && a<=90)
+//	    {
+//	    	System.out.println("it is capital letter");
+//	    }
+//	    else
+//	    {
+//	    	System.out.println("not a capital letter");
+//	    }
+	}
+     	
+}
+		
+		
