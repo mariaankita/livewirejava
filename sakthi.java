a54ae0be926b171d54e437e86dda34089d1aa2e3
mariaankita <mariaankita08@gmail.com>
2026-06-24 16:34:35 +0530

    Create sakthi.java

---
+++ sakthi.java
@@ -0,0 +1,184 @@
+package sakthivel;
+import java.util.Scanner;
+public class sakthi {
+	//static void square(int a)
+	//{
+//		int c=a*a;
+//		System.out.println(c);
+	//}
+	//static void cube(int b)
+	//{
+//		int d=b*b*b;
+//		System.out.println(d);	
+	//}
+//		public static void main(String[] args)
+//		{
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("enter a number to square...");
+//		int a=sc.nextInt();
+//		System.out.println("enter a number to cube...");
+//		int b=sc.nextInt();
+//		square(a);
+//		cube(b);
+	//	
+//	static void splitdigits(int a)
+//	{
+//		while(a>0)
+//		{
+//		int c=a%10;
+//		System.out.print(c+" ");
+//		a=a/10;
+//		}
+//	}
+//
+//	public static void main(String[] args)
+//	{
+//	  Scanner sc=new Scanner(System.in);
+//	  System.out.println("enter the number:");
+//	  int a=sc.nextInt();
+//	  splitdigits(a);
+//	}
+
+	
+//static int sum(int a)
+//{
+//	int sum=0;
+//	while(a>0)
+//		{
+//		int c=a%10;
+//		sum+=c;
+//	    a=a/10;
+//		}
+//	return sum;
+//}
+//static int multi(int a)
+//	{
+//	int multi=1;
+//	while(a>0)
+//	{
+//	int c=a%10;	
+//	multi*=c;
+//	a=a/10;
+//	}
+//	return multi;
+//}
+//public static void main(String[] args)
+//{
+//	Scanner sc=new Scanner(System.in);
+//	System.out.println("Enter the number:");
+//	int a=sc.nextInt();
+//	int s=sum(a);
+//	int m=multi(a);
+//	if (s==m)
+//	{
+//		System.out.println("Its a spy number");
+//	}
+//	else
+//	{
+//		System.out.println("not a spy number");
+//	}
+//}
+	
+//	static void squaredigit(int a)
+//		{
+//			while(a>0)
+//				{
+//				int c=a%10;
+//				int b=c*c;
+//			    a=a/10;
+//			    System.out.print(b+" , ");
+//				}
+//		}
+//public static void main(String[] args)
+//{
+//		
+//			Scanner sc=new Scanner(System.in);
+//			System.out.println("Enter the number:");
+//			int a=sc.nextInt();
+//			squaredigit(a);
+//}
+
+//static int count(int a)
+//{
+//	int coun=0;
+//	while(a>0)
+//	{
+//		coun+=1;
+//		a=a/10;
+//	}
+//	return coun;
+//	
+//	
+//}
+//public static void main(String[] args)
+//{
+//	Scanner sc=new Scanner(System.in);
+//	System.out.println("Enter the number:");
+//	int a=sc.nextInt();
+//	System.out.println(a+" is a "+count(a)+" digit number");
+//}
+
+//static int sumofdivisor(int a)
+//{
+//	int sum=0;
+//	for(int i=1;i<=a;i++)
+//	{
+//		if(a%i==0)
+//		{
+//			sum+=i;
+//		}
+//	}
+//	return sum;
+//}
+//public static void main(String[] args)
+//{
+//	Scanner sc=new Scanner(System.in);
+//	System.out.println("Enter the number:");
+//	int a=sc.nextInt();
+//	System.out.println(sumofdivisor(a));
+//}
+//}
+ 
+	
+//static void square(int a)
+//{
+//	int c=a*a;
+//	System.out.println(c);
+//}
+//static void cube(int b)
+//{
+//	int d=b*b*b;
+//	System.out.println(d);	
+//}
+//	public static void main(String[] args)
+//	{
+//	Scanner sc=new Scanner(System.in);
+//	System.out.println("enter a number to square...");
+//	int a=sc.nextInt();
+//	System.out.println("enter a number to cube...");
+//	int b=sc.nextInt();
+//	square(a);
+//	cube(b);
+//	
+// static void splitdigits(int a)
+// {
+// 	while(a>0)
+// 	{
+// 	int c=a%10;
+// 	System.out.print(c );
+// 	a=a/10;
+// 	}
+// }
+
+// public static void main(String[] args)
+// {
+//   Scanner sc=new Scanner(System.in);
+//   System.out.println("enter the number:");
+//   int a=sc.nextInt();
+//   splitdigits(a);
+// }
+// }
+
+		
+
+
