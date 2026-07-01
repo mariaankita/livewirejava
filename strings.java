a863ffa493eb9328235f0cf7a274b7734dbab24e
mariaankita <mariaankita08@gmail.com>
2026-07-01 16:34:52 +0530

    Create strings.java

---
+++ strings.java
@@ -0,0 +1,113 @@
+package Demo_task;
+
+public class strings {
+
+	public static void main(String[] args) {
+		
+//		  1	
+//			Scanner sc=new Scanner (System.in);
+//			System.out.println("enter yor string:");
+//			String s=sc.nextLine();
+//			System.out.println("enter your character:");
+//			char c=sc.next().charAt(0);
+//			char a[]=s.toCharArray();
+//			int count=0;
+//			for (char i:a)
+//			{
+//				if (i==c)
+//				{
+//					count++;
+//				}
+//				
+//			}
+//			System.out.println("found "+count+" times");
+			
+				
+//				2
+		//Scanner sc=new Scanner (System.in);
+		//System.out.println("enter yor string:");
+		//String s=sc.nextLine();
+		//char c[]={'a','e','i','o','u','A','E','I','O','U'};
+		//char a[]=s.toCharArray();
+		//int count=0;
+		//System.out.println("the vowels are:");
+		//for(int i=0;i<c.length;i++)
+		//  {
+//			for(int j=0;j<a.length;j++)
+//			{
+//				if (a[j]==c[i])
+//				   {
+//					   count++;
+//					   
+//					   System.out.println(a[j]);
+		//
+//				   }
+//			}
+		//   
+		//}
+		//System.out.println("no.of vowels: "+count);
+
+
+
+//				3
+		//Scanner sc=new Scanner (System.in);
+		//System.out.println("enter yor string:");
+		//String s=sc.nextLine();
+		//String u=s.toUpperCase();
+		//String a[]=u.split(" ");
+		//
+		//for (String i:a)
+		//{
+//			System.out.println(i);
+		//}
+
+
+//		      4
+		//Scanner sc=new Scanner (System.in);
+		//System.out.println("enter yor string:");
+		//String s=sc.nextLine();
+		//char c[]={'a','e','i','o','u','A','E','I','O','U'};
+		//char a[]=s.toCharArray();
+		//
+		//String s1=" ";
+		//for(int i=0;i<s.length();i++)
+		//{
+//			s1=s.charAt(i)+s1;
+		//}
+		//System.out.println(s1);
+		//int count=0;
+		//for(int i=0;i<c.length;i++)
+		//  {
+//			for(int j=0;j<a.length;j++)
+//			{
+//				if (a[j]==c[i])
+//				   {
+//					   count++;
+//				   }
+//			}
+		//   
+		//}
+		//System.out.println("no.of vowels: "+count);
+				
+		//Scanner sc=new Scanner (System.in);
+		//System.out.println("enter yor string:");
+		//String s=sc.nextLine();
+		//String[] word=s.split(" ");
+		//String max="";
+		//for (String words:word)
+		//{
+		//
+//			if(word.length>0)
+//			{
+//				max=words;
+//			}
+		//}
+		//System.out.println("The longest word is: "+max);
+//				
+//				
+//				
+		}	
+		}
+	
+
+
