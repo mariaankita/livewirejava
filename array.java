e36203a91dbad158c5b2dfc8e4ebedda2542f342
mariaankita <mariaankita08@gmail.com>
2026-07-22 16:15:56 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,87 @@
+package poorna1.java;
+
+import java.util.Scanner;
+
+public class array {
+
+	public static void main(String[] args) {
+//		Scanner sc= new Scanner(System.in);
+//		System.out.println("enter array size:...");
+//		int size =sc.nextInt();
+//		int a[]=new int[size];
+//		System.out.println("enter array elements..");
+//		for (int i=0;i<a.length;i++)
+//		{
+//			a[i]=sc.nextInt();
+//		}
+//		System.out.println("Array elements...");
+//		for (int i=0;i<a.length;i++)
+//		{
+//			for (int j=i+1;j<a.length;j++)
+//			{
+//				if (a[i]<a[j])
+//				{
+//					int temp=a[i];
+//					a[i]=a[j];
+//					a[j]=temp;
+//					
+//				}
+//				
+//			}
+//			System.out.println(a[i]);
+//			
+			
+//			Scanner sc= new Scanner(System.in);
+//			System.out.println("enter array size:...");
+//			int size =sc.nextInt();
+//			int a[]=new int[size];
+//			System.out.println("enter array elements..");
+//			for (int i=0;i<a.length;i++)
+//			{
+//				a[i]=sc.nextInt();
+//			}
+//			for (int i=0;i<a.length;i++)
+//			{
+//				for (int j=i+1;j<a.length;j++)
+//				{
+//					if (a[i]==a[j])
+//					{
+//						System.out.println("duplicate key: "+ a[i]);
+//					}
+//				}
+//			}
+	
+
+		
+//		Scanner sc= new Scanner(System.in);
+//		int od=0;
+//		int dd=0;
+//		int a[]=new int[5];
+//		System.out.println("enter array elements..");
+//		for (int i=0;i<a.length;i++)
+//		{
+//			a[i]=sc.nextInt();
+//		}	
+//		for (int i=0;i<a.length;i++)
+//		{
+//			if(a[i]<10)
+//			{
+//				od+=a[i];
+//			}
+//			else
+//		    {   
+//				dd+=a[i];
+//		    }
+//		}
+//		System.out.println("Sum of single digits numbers are: "+od);
+//		System.out.println("Sum of two-digit numbers are: "+dd);
+//		
+//		
+		
+		
+		
+		
+	}
+}
+
+
